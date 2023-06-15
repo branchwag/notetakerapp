be8f263056798b685a121a0465cc1be60242b14f
LeAnne B <branchleannelmb@gmail.com>
2023-06-15 16:50:49 -0400

    adds in working routes

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Import the feedback router
+// const api = require('./routes/index');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to serve up static assets from the public folder
+app.use(express.static('public'));
+
+//post request
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      var notes = JSON.parse(data);
+      let userNote = req.body;
+      userNote.id = Math.floor(Math.random() * 5000);
+      notes.push(userNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes), (err, data) => {
+        res.json(userNote);
+    });
+    }); 
+  });
+
+//delete req
+  app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      let notes = JSON.parse(data);
+      const newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
+    
+    fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
+      res.json({msg: 'successful'});
+    });
+  });
+  });
+
+  //GET
+  app.get('api/notes/:id', (req, res) =>{
+    res.json(notes[req.params.id]);
+  });
+
+  //GET
+  app.get('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      var notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+  
+  app.get('/notes', (req, res) => {
+      res.sendFile(path.join(__dirname, '/public/notes.html'))
+  });
+  
+  app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname, '/public/index.html'));
+  });   
+  
+  app.listen(PORT, () => {
+      console.log(`App listening at http://localhost:${PORT} 🚀`)
+  });
+  
+  
